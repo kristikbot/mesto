878b44c4fca0161fb273158da4a8aca07db080a7
Kristikbot <kristikbot@yandex.ru>
2020-09-29 20:59:43 +0300

    save changes

---
+++ script.js
@@ -0,0 +1,3 @@
+const buttonOpenEdit = document.querySelector(".profile__edit-btn");
+const buttonCloseEdit = document.querySelector(".popup__close");
+const buttonSaveEdit = document.querySelector(".popup__save");
