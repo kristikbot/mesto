3e492bdde510fac877c145b1b748a8a78893c46e
Kristikbot <kristikbot@yandex.ru>
2020-09-30 01:43:00 +0300

    const set

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const buttonOpenEdit = document.querySelector(".profile__edit-btn");
 const buttonCloseEdit = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
 
+const inputName = document.querySelector('.popup__text_name');
+const inputAbout = document.querySelector('.ppopup__text_about');
+
 function openPopup() {
     popup.classList.add("popup__opened");
     popup.setAttribute("style", "display:flex");
@@ -17,24 +20,15 @@ function closePopup() {
   }
   buttonCloseEdit.addEventListener("click", closePopup)
 
+
+  console.log(typeof document.querySelector('.popup__text_name').value);
+
 /*
 const popupToggle = () => {
-    popup.classList.toggle(".popup__opened")
+    console.log("#1");
+    popup.classList.toggle("popup__opened")
 }
 
-buttonOpenEdit.addEventListener("click", popupToggle)
-buttonCloseEdit.addEventListener("click", popupToggle)
-
-
-function editProfile() {
-    popup.classList.add("popup__opened")
-}
-
-buttonOpenEdit.addEventListener("click", editProfile)
-
-
-
-/*
 buttonOpenEdit.addEventListener("click", popupToggle)
 buttonCloseEdit.addEventListener("click", popupToggle)
 
