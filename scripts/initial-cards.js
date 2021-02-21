623435755f23384aeec5f2db0af833206efbbce0
Kristikbot <kristikbot@yandex.ru>
2021-02-22 00:44:55 +0900

    initial cards

---
+++ scripts/initial-cards.js
@@ -0,0 +1,27 @@
+//дефолтные карточки
+const initialCards = [
+    {
+        name: 'Maja Ruznic',
+        link: 'https://i.pinimg.com/564x/69/d7/ff/69d7ff060aa1bfc5089d8b76b9996c48.jpg'
+    },
+    {
+        name: 'Richard Butler',
+        link: 'https://i.pinimg.com/564x/bc/84/be/bc84be5d9f411234728942098cfb3de3.jpg'
+    },
+    {
+        name: 'Naudline Pierre',
+        link: 'https://i.pinimg.com/564x/23/c8/02/23c8022b2c1074fcefbd3e3aa8779869.jpg'
+    },
+    {
+        name: 'Vivian Springford',
+        link: 'https://i.pinimg.com/564x/29/e9/c7/29e9c767f9386735f1dde59cfd3d7ac2.jpg'
+    },
+    {
+        name: 'Larry Poons',
+        link: 'https://i.pinimg.com/564x/af/d3/3c/afd33c680d406918eaab7b7adcfc05d4.jpg'
+    },
+    {
+        name: 'James Marshall',
+        link: 'https://i.pinimg.com/564x/66/04/39/6604391f91e0e91e93e4243a63862437.jpg'
+    }
+  ]; 
